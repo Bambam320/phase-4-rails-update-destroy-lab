d0fb96a5c20ba1abd932a57e7ea43e8386742fcc
Bambam320 <igor.mestechkin@gmail.com>
2022-11-10 20:57:03 -0500

    Completed Assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,8 +18,25 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = plant_find
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = plant_find
+    plant.destroy
+    head :no_content
+  end
+
   private
 
+  def plant_find
+    Plant.find(params[:id])
+  end
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
